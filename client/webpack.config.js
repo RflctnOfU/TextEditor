da8b116b1d0b30d0cb69751f8755a72515d984f8
Kris Marshall <rflctnofu@hotmail.com>
2022-08-18 16:18:28 -0400

    get webpack.config setup

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,41 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new InjectManifest({
+        swSrc: './src/sw.js',
+        swDest: 'service-worker.js',
+      }),
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Just Another Text Editor',
+      }),
+      new WebpackPwaManifest({
+        short_name: "",
+        name: "",
+        icons: [
+          {
+
+          }
+        ]
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
       ],
     },
   };
