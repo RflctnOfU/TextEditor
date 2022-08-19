42ab3508126a936001c17416ca0163118acac298
Kris Marshall <rflctnofu@hotmail.com>
2022-08-19 17:37:11 -0400

    bug went away ??

--- client/src-sw.js
+++ client/src-sw.js
@@ -29,6 +29,7 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 // TODO: Implement asset caching
 registerRoute(
   warmStrategyCache,
+  offlineFallback,
   new StaleWhileRevalidate({
     plugins: [
       new CacheableResponsePlugin({
@@ -38,4 +39,3 @@ registerRoute(
   })
 
 );
-
